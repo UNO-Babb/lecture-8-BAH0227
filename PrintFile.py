caf3ebd7678f0224317f294e53bc4e3f5fcd061d
BAH0227 <brockahoover@gmail.com>
2025-04-07 00:06:38 +0000

    Finished

--- PrintFile.py
+++ PrintFile.py
@@ -2,9 +2,11 @@ def main():
   myFile = open("qbdata.txt", 'r')
 
   for line in myFile:
-    print (line)
+    info = line.split(" ")
+    print(info[0], info[1], "had a rating of", )
 
   myFile.close()
+
 
 
 if __name__ == '__main__':
